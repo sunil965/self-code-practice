652afcd88644abf9ce97a2d07f35bad99278e433
sunil956 <sunilkumar192114@gmail.com>
2023-01-20 08:57:55 +0530

    Bug fix.

--- code-2023/src/interview/mastercard/IndexOfXInSortedRotatedArray.java
+++ code-2023/src/interview/mastercard/IndexOfXInSortedRotatedArray.java
@@ -1,6 +1,6 @@
 package interview.mastercard;
 
-public class Test {
+public class IndexOfXInSortedRotatedArray {
 
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 5, 5, 5, 67, 123, 125};
@@ -20,9 +20,9 @@ public class Test {
         if (x == arr[mid] && (mid == arr.length - 1 || x < arr[mid + 1]))
             return mid;
         else if (x < arr[mid]) {
-            return findFirstOccurrence(arr, x, start, mid - 1);
+            return lastFirstOccurrence(arr, x, start, mid - 1);
         } else {
-            return findFirstOccurrence(arr, x, mid + 1, end);
+            return lastFirstOccurrence(arr, x, mid + 1, end);
         }
     }
 
